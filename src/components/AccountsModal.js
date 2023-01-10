0183dd41bf543f34359f1ef6989dc58afa6bd882
trum <bardabarda228@gmail.com>
2023-01-10 13:27:09 +0500

    [+] Modal logic

--- src/components/AccountsModal.js
+++ src/components/AccountsModal.js
@@ -1,4 +1,8 @@
 import {Box, Button, Modal, TextField, Typography} from "@mui/material";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {addAddressList, setNewAddressList} from "../redux/actions/settingsActions";
+import uniqid from "uniqid";
 
 const style = {
     position: 'absolute',
@@ -12,10 +16,51 @@ const style = {
     p: 4,
 };
 
-function AccountsModal(props) {
+function AccountsModal({open, setOpenModal}) {
+    const [data, setData] = useState("");
+    const dispatch = useDispatch();
+
+    const handleCloseModal = () => {
+        setData("");
+        setOpenModal(false);
+    }
+
+    const handleChangeData = (e) => {
+        setData(e.target.value);
+    }
+
+    const handleAddNewAddressList = () => {
+        const splitArr = data.split('\n');
+        const arr = splitArr.map(address => {
+            const id = uniqid();
+            return {
+                id,
+                address
+            }
+        })
+        dispatch(addAddressList(arr));
+        setData("");
+        setOpenModal(false);
+    }
+
+    const handleSetNewAddressList = () => {
+        const splitArr = data.split('\n');
+        const arr = splitArr.map(address => {
+            const id = uniqid();
+            return {
+                id,
+                address
+            }
+        })
+        dispatch(setNewAddressList(arr));
+        setData("");
+        setOpenModal(false);
+    }
+
     return (
         <Modal
-            open={false}
+            open={open}
+            onClose={handleCloseModal}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
@@ -27,6 +72,9 @@ function AccountsModal(props) {
                     multiline
                     rows={12}
                     fullWidth
+                    autoFocus
+                    value={data}
+                    onChange={handleChangeData}
                     placeholder="bumbalumba35
 bumbalumba34
 wa1xw.wam
@@ -35,14 +83,16 @@ waaiw.wam
 wazxw.wam
 hwkxw.wam"
                 />
-                <div style={{ display: "flex", justifyContent: "start", position: "relative" }}>
-                    <Button variant="outlined" sx={{ marginTop: "10px", marginRight: "10px" }}>
+                <div style={{display: "flex", justifyContent: "start", position: "relative"}}>
+                    <Button onClick={handleAddNewAddressList} variant="outlined" sx={{marginTop: "10px", marginRight: "10px"}}>
                         Add
                     </Button>
-                    <Button variant="outlined" color="secondary" sx={{ marginTop: "10px", marginRight: "10px" }}>
+                    <Button onClick={handleSetNewAddressList} variant="outlined" color="secondary" sx={{marginTop: "10px", marginRight: "10px"}}>
                         Save as new
                     </Button>
-                    <Button variant="outlined" color="error" sx={{ marginTop: "10px", position: "absolute", right: "0px" }}>
+                    <Button variant="outlined" color="error"
+                            onClick={handleCloseModal}
+                            sx={{marginTop: "10px", position: "absolute", right: "0px"}}>
                         Close
                     </Button>
                 </div>
