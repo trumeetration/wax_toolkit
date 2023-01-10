ae5a9cf6f95d9d0f0fe19bc20f9b1392254c586c
trum <bardabarda228@gmail.com>
2023-01-10 13:26:36 +0500

    [+] Modal logic

--- src/components/AccountCard.js
+++ src/components/AccountCard.js
@@ -1,14 +1,24 @@
 import {Card, Grid, IconButton, Typography} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import {useDispatch} from "react-redux";
+import {deleteAddress} from "../redux/actions/settingsActions";
 
 
 function AccountCard({ data }) {
+    const { id, address } = data;
+    const dispatch = useDispatch();
+
+    const handleDeleteAddress = (e) => {
+        e.preventDefault();
+        dispatch(deleteAddress(id));
+    }
+
     return (
         <Grid item>
             <Card variant="outlined" sx={{position: "relative"}}>
                 <div style={{padding: "10px", display: "flex", justifyContent: "space-between"}}>
-                    <Typography>{data.address}</Typography>
-                    <IconButton type="submit" sx={{padding: "0", marginLeft: "5px"}}>
+                    <Typography>{address}</Typography>
+                    <IconButton onClick={handleDeleteAddress} type="submit" sx={{padding: "0", marginLeft: "5px"}}>
                         <CloseIcon fontSize="small"/>
                     </IconButton>
                 </div>
